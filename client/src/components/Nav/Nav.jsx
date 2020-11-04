2c949a1f4edcebb8afabea36582caa9ac2afd2e9
P0rtt3r <franciscomanuel.gonzalez3@gmail.com>
2020-11-04 17:15:42 -0300

    s7: SearchBar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,12 +3,13 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+
+import SearchBar from '../SearchBar/SearchBar'
+
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -150,21 +151,7 @@ export default function PrimarySearchAppBar() {
             </IconButton>
           </div>
 
-            {/* search bar */}
-            <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                <SearchIcon />
-                </div>
-                <InputBase
-                placeholder="Search…"
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-                />
-                
-            </div>
+            <SearchBar classes={classes}/>
 
         </Toolbar>
       </AppBar>
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,50 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const SearchBar = ({onSearch}) => {
-  const [funko, setFunko] = useState("");
+// const SearchBar = ({onSearch}) => {
+//   const [funko, setFunko] = useState("");
+//   return (
+//     <form onSubmit={(e) => {
+//       e.preventDefault();
+//       onSearch(funko);
+//     }}>
+//       <input
+//         type="text"
+//         placeholder="Funko..."
+//         value={funko}
+//         onChange={e => setFunko(e.target.value)}
+//       />
+//       <input type="submit" value="Search" />
+//     </form>
+//   );
+// }
+
+// export default SearchBar;
+
+
+import React, { useState } from "react";
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+
+const SearchBar = ({classes}) => {
+
+  
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(funko);
-    }}>
-      <input
-        type="text"
-        placeholder="Funko..."
-        value={funko}
-        onChange={e => setFunko(e.target.value)}
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+      placeholder="Search…"
+      classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+      }}
+      inputProps={{ 'aria-label': 'search' }}
       />
-      <input type="submit" value="Search" />
-    </form>
+      
+  </div>
   );
 }
 
 export default SearchBar;
+
