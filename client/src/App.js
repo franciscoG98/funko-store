49dec0988cca7bd48651dd8c9cd37bab469fac37
juanabrate <juan.abrate21@gmail.com>
2020-11-06 19:31:14 -0300

    base de routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import Products from './components/Products/Products';
 import './App.css';
 import Nav from '../src/components/Nav/Nav';
 import CRUForm from '../src/components/CRUForm/CRUForm';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
 
-      <Nav />
-      <Products />
-      <CRUForm />
+     <BrowserRouter>
+
+      <Route path="/" component= {Nav} />      
+      <Route path="/" component= {Products} />
+      <Route path="/" component= {CRUForm} />
+
+     </BrowserRouter>
       
     </div>
   );
