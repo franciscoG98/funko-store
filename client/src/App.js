e26bbb879f618eecf783a4c82918d47d7eb6b596
juanabrate <juan.abrate21@gmail.com>
2020-11-07 18:49:44 -0300

    ruteado2

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,29 @@ import React from 'react';
 import Products from './components/Products/Products';
 import './App.css';
 import Nav from '../src/components/Nav/Nav';
-import CRUForm from '../src/components/CRUForm/CRUForm';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Categories from './components/Admin/Categories/Categories';
-import FormDialog from './components/Admin/Categories/DialogCRUDCat';
 import ListProducts from "./components/Admin/Products/listProducts";
-import AddProduct from '../src/components/CRUForm/AddProduct';
+
 
 function App() {
   return (
     <div className="App">
 
-      <BroserRouter> 
+      <BrowserRouter> 
 
         <Nav />
-        <Products />
-        <FormDialog />
-        <Categories />
-        <AddProduct />
-        <ListProducts/>
+
+
+           < Route exact path= '/' component= {Products} />
+
+           < Route exact path= '/admin/categories' component= {Categories} />
+
+           < Route exact path= '/admin/products' component= {ListProducts} />        
+        
       
-      </BroserRouter>
-            
+      </BrowserRouter>
+
 
     </div>
   );
