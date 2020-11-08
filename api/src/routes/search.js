a580146e21494dc32ea36867333552fc4c6e40c8
julicheruse <julicheruse@gmail.com>
2020-11-08 17:11:13 -0300

    ruta search query

--- api/src/routes/search.js
+++ api/src/routes/search.js
@@ -1,13 +1,25 @@
 const server = require('express').Router();
 const { Product, Categories } = require('../db.js');
+const { Op } = require("sequelize");
 
 server.get('/', (req, res, next) => {
     const {query} = req.query
-	
+    //console.log(typeof query)
 	
 	 Product.findAll({
-       where: {name: query}
-	  })
+       where: {
+        [Op.or]: [
+          {
+            name:{
+              [Op.substring]: `${query}`
+            }
+          }, 
+          {
+            description: {
+              [Op.substring]: `${query}`  
+            }
+       }] 
+	  }})
 		.then(products => {
 			res.send(products);
 		})
