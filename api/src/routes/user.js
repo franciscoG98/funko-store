7231997df8bb95c3ebec754364ee4602a87f2d15
flor-20 <f.mignini@hotmail.com>
2020-11-11 08:52:35 -0300

    creacion ruta usuario

---
+++ api/src/routes/user.js
@@ -0,0 +1,34 @@
+const server = require('express').Router();
+const { User } = require('../db.js');
+//const { Op } = require("sequelize");
+
+//route for a user creation depending on if is admin or not.
+    server.post('/', function (req, res, next) {
+        let user;
+
+        let { fullname, email, phone, address, purchase, isAdmin} = req.body;
+
+        if(!isAdmin){
+            if (!fullname || !email || !address || !phone || !purchase) {
+                return res.status(400).json({ msg: "Invalid or missing data" });
+              }
+        }else{
+            User.create({
+                fullname,
+                email,
+                phone,
+                address,
+                purchase
+            })
+            .then(
+                (userCreated) => 
+            { res.status(200).json({ msg: "User created", user: userCreated })
+            })
+            .catch(next);
+        }
+        
+       
+    
+});
+
+module.exports = server;
