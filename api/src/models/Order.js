24f4e9dc6423a0524624bd885df5f0b2a2a43b8f
julicheruse <julicheruse@gmail.com>
2020-11-14 15:55:31 -0300

    Ruta GET, POST, PUT de Cart ANDAN

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -6,10 +6,6 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      /* quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },  */ 
       state: {
         type: DataTypes.ENUM('cart', 'created', 'processing', 'canceled', 'completed'),
         allowNull: false,
