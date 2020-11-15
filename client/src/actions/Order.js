c730afdefb77115d6c7fd1d4413e05a216ec9631
flor-20 <f.mignini@hotmail.com>
2020-11-15 16:18:00 -0300

    UserOrder

--- client/src/actions/Order.js
+++ client/src/actions/Order.js
@@ -48,7 +48,7 @@ export function getAdminOrders() {
 //traer ordenes para el componente UserOrdenList
 export function getUserOrders() {
     return (dispatch) => {
-        return Axios.get(`http://localhost:3001/user`)
+        return Axios.get(`http://localhost:3001/1/cart`)
             .then(json => {
                 dispatch({ type: 'GET_USER_ORDER', payload: json });
             })
