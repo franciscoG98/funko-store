95c36e5c07de26d1e83d88e66f365f9775146d27
flor-20 <f.mignini@hotmail.com>
2020-11-17 16:10:06 -0300

    reset redux

--- client/src/actions/Login.js
+++ client/src/actions/Login.js
@@ -1,12 +1,21 @@
 import Axios from 'axios';
 
 
+//falta la ruta y probar!!!!
+// export function loginUser(login) {
+//     return (dispatch) => {
+//         return Axios.post(`http://localhost:3001/`, login)
+//             .then(json => {
+//                 dispatch({ type: "LOGIN_USER", payload: json });
+//             });
+//     }
+// }
 
-export function loginUser(login) {
+export function passwordReset(id) {
     return (dispatch) => {
-        return Axios.post(`http://localhost:3001/`, login)
+        return Axios.post(`http://localhost:3001/${id}/passwordReset`)
             .then(json => {
-                dispatch({ type: "LOGIN_USER", payload: json });
+                dispatch({ type: "RESET_PASSWORD", payload: json });
             });
     }
 }
