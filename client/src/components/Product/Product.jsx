ca5bfa5d856bfb1b3ffa0c0150959cee64a64262
juanabrate <juan.abrate21@gmail.com>
2020-11-20 15:13:44 -0300

    cards

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -26,6 +26,7 @@ import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
 import DetailModal from '../DetailModal/DetailModal'
+import { green } from '@material-ui/core/colors';
 
 
 const Product = ({f}) => {
@@ -57,53 +58,57 @@ const Product = ({f}) => {
           title={f.name}
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {f.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           <span style={{fontWeight: "bold"}}> Description: </span> {f.description}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          <span style={{fontWeight: "bold"}}> Price: </span> ${f.price}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {f.stock > 0 ? <span>Diponible</span> : <span style={{color: "red"}}>No hay unidades disponibles</span>}
-          </Typography>
-            <br/>
+            <Typography style={{fontFamily: 'Ubuntu', fontSize: '30px', fontWeight: 'light', fontStyle: 'normal'}} gutterBottom variant="h5" component="h2">
+                {f.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                <span style={{fontFamily: 'Raleway', fontWeight: "bolder", marginLeft: '1px', fontSize: '15px', color: '#585858'}}> Description: </span> 
+                  <span style={{fontFamily: 'Calibri', marginLeft: '1px', fontSize: '15px', color: '#686868'}}>
+                    {f.description}
+                  </span>
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                <span style={{fontFamily: 'Raleway', fontWeight: "bolder  ", marginLeft: '1px', fontSize: '15px', color: '#585858'}}> Price: </span> 
+                <span style={{fontFamily: 'Raleway', marginLeft: '1px', fontSize: '13px', color: '#585858', fontWeight: "bolder"}}> $ </span> 
+                <span style={{fontFamily: 'Calibri', marginRight: '3px', fontSize: '16px', color: '#585858'}}> {f.price} </span>
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {f.stock > 0 ? <span style= {{opacity: '100%', color: '#33CC33', fontFamily:'Calibri', fontSize: '17px'}}>Available</span> : <span style={{color: "red", marginLeft: '1px', fontFamily:'Calibri', fontSize: '17px'}}>No Stock</span>}
+            </Typography>
+                <br/>
             <Reviews />
-
         </CardContent>
       </CardActionArea>
       <CardActions>
-      {f.stock > 0 ? <Button size="small" color="primary" onClick={() => dispatch( addItem(f.id) )}>
-        <AddShoppingCartRoundedIcon />
-          Add To Cart 
-        </Button>: null}
-        <Button size="small" color="primary" onClick={() => handleOpen()}>
-            <OpenInNewRoundedIcon />
-            More 
-        </Button>
+              {f.stock > 0 ? <Button style={{color: '#585858', opacity: '100%'}} size="small" color="primary" onClick={() => dispatch( addItem(f.id) )}>
+            <AddShoppingCartRoundedIcon />
+              Add To Cart 
+            </Button>: null}
+            <Button style={{color: '#585858'}} size="small" color="primary" onClick={() => handleOpen()}>
+                <OpenInNewRoundedIcon />
+                More 
+            </Button>
 
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className={classes.modal}
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-          >
-          <Fade in={open}>
-            <div className={classes.paper}>
-              <DetailModal f={f}/>
-            </div>
-          </Fade>
-        </Modal>
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              className={classes.modal}
+              open={open}
+              onClose={handleClose}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+              >
+              <Fade in={open}>
+                <div className={classes.paper}>
+                  <DetailModal f={f}/>
+                </div>
+              </Fade>
+            </Modal>
 
-      </CardActions>
+        </CardActions>
     </Card>
   );
 }
