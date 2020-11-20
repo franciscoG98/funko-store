4b933d58ae1b9c8608765b0b6fc4e7e9904b352e
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2020-11-20 10:23:37 -0300

    borro packages

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -2,7 +2,14 @@ const server = require('express').Router();
 const bcrypt = require("bcrypt");
 const { User } = require('../db.js');
 
+// isAuthenticated
+
 server.post('/login', (req, res) => {
+    const { email } = req.body;
+    const { passwd } = req.body;
+
+    res.send(console.log('email', email, 'password', passwd))
+
 
 })
 
