ec16c56901c0f863b92a47b992683e3e8779aa38
flor-20 <f.mignini@hotmail.com>
2020-11-26 11:13:07 -0300

    change

--- client/src/components/saveToLocalStorage/LocalStorage.js
+++ client/src/components/saveToLocalStorage/LocalStorage.js
@@ -1,21 +1,26 @@
 
 export const loadState = () => {
     try {
-        const serializedData = localStorage.getItem("guest");
-        if (serializedData === null) {
+        const serializedCarrito = localStorage.getItem("carritoGuest");
+        const serializedUser = localStorage.getItem("user");
+        if (serializedCarrito === null && serializedUser === null) {
             return undefined; // Si no existe el state en el local storage devolvemos undefined para que cargue el state inicial que hayamos definido
         }
-        return JSON.parse(serializedData); // Si encontramos con exito nuestro storage lo devolvemos.
+        if (serializedCarrito) {
+            return JSON.parse(serializedCarrito); // Si encontramos con exito nuestro storage lo devolvemos.
+        }
     } catch (error) {
         return undefined; // Si ocurre algun error, devuelvo undefined para cargar el state inicial.
     }
 };
-export const saveState = (prod) => {
-
+export const saveState = (prod, user) => {
+    console.log(user)
     try {
-        let serializedData = JSON.stringify(prod);
-        localStorage.setItem("guest", serializedData);
+        let serializedCarrito = JSON.stringify(prod);
+        // let serializedUser = JSON.stringify(user);
+        localStorage.setItem("carritoGuest", serializedCarrito);
+        // localStorage.setItem("user", serializedUser);
     } catch (error) {
-        // Acá podemos capturar o crear cualquier log que deseemos en caso de que falle el salvado en el storage.
+        console.log(error)// Acá podemos capturar o crear cualquier log que deseemos en caso de que falle el salvado en el storage.
     }
 };
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,43 +3,19 @@ import Reducer from "../reducers";
 import thunk from "redux-thunk";
 import { loadState, saveState } from '../components/saveToLocalStorage/LocalStorage';
 
-function saveToLocalStorage(state) {
-    try {
-        const serializedState = JSON.stringify(state)
-        localStorage.setItem('state', serializedState)
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-function loadFromLocalStorage() {
-    try {
-        const serializedState = localStorage.getItem('state')
-        if (serializedState === null) return undefined
-        return JSON.parse(serializedState)
-    } catch (e) {
-        console.log(e)
-        return undefined
-    }
-}
-
-
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const initialData = loadState()
-// const persistedState = loadFromLocalStorage();
+
 
 const store = createStore(
     Reducer,
-    // persistedState,
     initialData,
     composeEnhancers(applyMiddleware(thunk))
 );
 
-// store.subscribe(() => saveToLocalStorage(store.getState()));
 store.subscribe(function () {
-    saveState(store.getState().Order.guestCart)
+    saveState(store.getState())
 })
 
 export default store;
